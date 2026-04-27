0cff833b325d7e2b65018912dadcf3a4e0b66a53
RynnKT <ontab100@gmail.com>
2026-04-27 12:37:21 +0700

    Nilai

---
+++ src/main/java/com/mycompany/mahasiswa/Mahasiswa.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa;
+
+/**
+ *
+ * @author Ridho
+ */
+public class Mahasiswa {
+    private String nama;
+    private int nilai;
+
+    // constructor
+    public Mahasiswa(String nama, int nilai) {
+        this.nama = nama;
+        this.nilai = nilai;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    // getter (optional tapi bagus)
+    public int getNilai() {
+        return nilai;
+    }
+
+    // method utama
+    public boolean isLulus() {
+        return nilai >= 60;
+    }
+}
---
+++ src/test/java/com/mycompany/mahasiswa/MahasiswaTest.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.mahasiswa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ *
+ * @author Ridho
+ */
+public class MahasiswaTest {
+    
+    public MahasiswaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNama method, of class Mahasiswa.
+     */
+ 
+    /**
+     * Test of getNilai method, of class Mahasiswa.
+     */
+ 
+    @Test
+    void testLulus() {
+        Mahasiswa mhs = new Mahasiswa("Budi", 75);
+        assertTrue(mhs.isLulus());
+    }
+
+    @Test
+    void testTidakLulus() {
+        Mahasiswa mhs = new Mahasiswa("Andi", 50);
+        assertFalse(mhs.isLulus());
+    }
+    
+}
